642c46591914e2fc0362ec269e915f8c520d3fd7
Will Graham <willgraham345@gmail.com>
2025-03-02 14:56:29 -0700

    Made personal up to date with work

--- personal/nvim/lua/config/keymaps.lua
+++ personal/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- vim.keymap.set(
+--   "n",
+--   "<M-N>",
+--   "<Cmd>BufferLineCyclePrev<CR>",
+-- )
+vim.keymap.set("n", "<M-e>", "<cmd>Neotree focus<cr>", { desc = "Neotree focus" })
+vim.keymap.set("n", "<Space>wz", "<cmd>Toggle zoom mode<cr>", { desc = "Toggle Zoom mode" })
