2b5142ea2bd3f712c49d5f8372f6e4ac2b799475
Will Graham <willgraham345@gmail.com>
2025-03-08 12:02:38 -0700

    Updated nvim config from work to personal and server

--- personal/nvim/lua/plugins/colorscheme.lua
+++ personal/nvim/lua/plugins/colorscheme.lua
@@ -1,45 +1,72 @@
 return {
-  { "navarasu/onedark.nvim" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      style = "dark", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-      transparent = false, -- Show/hide background
-      term_colors = true, -- Change terminal color as per the selected theme style
-      ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-      cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-
-      -- toggle theme style ---
-      toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-      toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
-
-      -- Change code style ---
-      -- Options are italic, bold, underline, none
-      -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
-      code_style = {
-        comments = "italic",
-        keywords = "none",
-        functions = "none",
-        strings = "none",
-        variables = "none",
-      },
-
-      -- Lualine options --
-      lualine = {
-        transparent = false, -- lualine center bar transparency
-      },
-
-      -- Custom Highlights --
-      colors = {}, -- Override default colors
-      highlights = {}, -- Override highlight groups
-
-      -- Plugins Config --
-      diagnostics = {
-        darker = true, -- darker colors for diagnostic
-        undercurl = true, -- use undercurl instead of underline for diagnostics
-        background = true, -- use background color for virtual text
-      },
-      colorscheme = "onedark",
-    },
+  "olimorris/onedarkpro.nvim",
+  priority = 1000, -- Ensure it loads first
+  opts = {
+    cursorline = true, -- Use cursorline highlighting?
+    transparency = false, -- Use a transparent background?
+    terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
+    lualine_transparency = false, -- Center bar transparency?
+    highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
+  },
+  styles = {
+    types = "NONE",
+    methods = "NONE",
+    numbers = "NONE",
+    strings = "NONE",
+    comments = "italic",
+    keywords = "bold,italic",
+    constants = "NONE",
+    functions = "italic",
+    operators = "NONE",
+    variables = "NONE",
+    parameters = "NONE",
+    conditionals = "italic",
+    virtual_text = "NONE",
   },
 }
+
+-- return {
+--   { "navarasu/onedark.nvim" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       style = "dark", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+--       transparent = false, -- Show/hide background
+--       term_colors = true, -- Change terminal color as per the selected theme style
+--       ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+--       cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+--
+--       -- toggle theme style ---
+--       toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+--       toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
+--
+--       -- Change code style ---
+--       -- Options are italic, bold, underline, none
+--       -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+--       code_style = {
+--         comments = "italic",
+--         keywords = "none",
+--         functions = "none",
+--         strings = "none",
+--         variables = "none",
+--       },
+--
+--       -- Lualine options --
+--       lualine = {
+--         transparent = true, -- lualine center bar transparency
+--       },
+--
+--       -- Custom Highlights --
+--       colors = {}, -- Override default colors
+--       highlights = {}, -- Override highlight groups
+--
+--       -- Plugins Config --
+--       diagnostics = {
+--         darker = true, -- darker colors for diagnostic
+--         undercurl = true, -- use undercurl instead of underline for diagnostics
+--         background = true, -- use background color for virtual text
+--       },
+--       colorscheme = "onedark",
+--     },
+--   },
+-- }
--- personal/nvim/lua/user/vscode_keymaps.lua
+++ personal/nvim/lua/user/vscode_keymaps.lua
@@ -1,20 +1,29 @@
-local keymap = vim.keymap.set
+local keymap = map
 local opts = { noremap = true, silent = true }
 
 -- remap leader key
-vim.keymap.set("n", "<Space>", "<Nop>")
+map("n", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-keymap({ "n", "v" }, "<leader>j", "<cmd> lua require('vscode').action('workbench.action.showCommands')<CR>")
 keymap(
   { "n", "v" },
-  "<leader>bo",
-  "<cmd> lua require('vscode').action('workbench.action.closeEditorsInOtherGroups')<CR>"
+  "<leader>j",
+  "<cmd> lua require('vscode').action('workbench.ac/home/will/repos/stoneward/.cacheshowCommands')<CR>"
 )
+-- buffers
+
 keymap({ "n", "v" }, "gk", "<cmd> lua require('vscode').action('showHover')<CR>")
 keymap({ "n", "v" }, "K", "<cmd> lua require('vscode').action('showHover')<CR>")
-keymap({ "n", "v" }, "L", "<cmd> lua require('vscode').action('workbench.action.nextEditor')<CR>")
-keymap({ "n", "v" }, "H", "<cmd> lua require('vscode').action('workbench.action.previousEditor')<CR>")
+
+-- Code navigation keymaps
+-- TODO: Figure out how the movement works in vscode, and where it's different in neovim
+-- keymap({ "n", "v" }, "gR", "<cmd> lua require('vscode').action('editor.action.referenceSerch.trigger')<CR>")
+-- keymap({ "n", "v" }, "gy", "<cmd> lua require('vscode').action('editor.action.peekDeclaration')<CR>")
+-- keymap({ "n", "v" }, "gY", "<cmd> lua require('vscode').action('editor.action.revealDeclaration')<CR>")
+-- TODO: Add stuff for collapsing/folding various stuff
+
+-- keymap({ "n", "v" }, "L", "<cmd> lua require('vscode').action('workbench.action.nextEditor')<CR>")
+-- keymap({ "n", "v" }, "H", "<cmd> lua require('vscode').action('workbench.action.previousEditor')<CR>")
 -- -- yank to system clipboard
 -- keymap({ "n", "v" }, "<leader>y", '"+y', opts)
 
@@ -41,32 +50,32 @@ keymap({ "n", "v" }, "H", "<cmd> lua require('vscode').action('workbench.action.
 -- call vscode commands from neovim
 
 -- general keymaps
-keymap({ "n", "v" }, "<leader>t", "<cmd>lua require('vscode').action('workbench.action.terminal.toggleTerminal')<CR>")
-keymap({ "n", "v" }, "<leader>b", "<cmd>lua require('vscode').action('editor.debug.action.toggleBreakpoint')<CR>")
-keymap({ "n", "v" }, "<leader>d", "<cmd>lua require('vscode').action('editor.action.showHover')<CR>")
-keymap({ "n", "v" }, "<leader>a", "<cmd>lua require('vscode').action('editor.action.quickFix')<CR>")
-keymap({ "n", "v" }, "<leader>sp", "<cmd>lua require('vscode').action('workbench.actions.view.problems')<CR>")
-keymap({ "n", "v" }, "<leader>cn", "<cmd>lua require('vscode').action('notifications.clearAll')<CR>")
-keymap({ "n", "v" }, "<leader>ff", "<cmd>lua require('vscode').action('workbench.action.quickOpen')<CR>")
-keymap({ "n", "v" }, "<leader>cp", "<cmd>lua require('vscode').action('workbench.action.showCommands')<CR>")
-keymap({ "n", "v" }, "<leader>pr", "<cmd>lua require('vscode').action('code-runner.run')<CR>")
-keymap({ "n", "v" }, "<leader>fd", "<cmd>lua require('vscode').action('editor.action.formatDocument')<CR>")
+-- keymap({ "n", "v" }, "<leader>t", "<cmd>lua require('vscode').action('workbench.action.terminal.toggleTerminal')<CR>")
+-- keymap({ "n", "v" }, "<leader>b", "<cmd>lua require('vscode').action('editor.debug.action.toggleBreakpoint')<CR>")
+-- -- keymap({ "n", "v" }, "<leader>d", "<cmd>lua require('vscode').action('editor.action.showHover')<CR>")
+-- keymap({ "n", "v" }, "<leader>a", "<cmd>lua require('vscode').action('editor.action.quickFix')<CR>")
+-- keymap({ "n", "v" }, "<leader>sp", "<cmd>lua require('vscode').action('workbench.actions.view.problems')<CR>")
+-- keymap({ "n", "v" }, "<leader>cn", "<cmd>lua require('vscode').action('notifications.clearAll')<CR>")
+-- keymap({ "n", "v" }, "<leader>ff", "<cmd>lua require('vscode').action('workbench.action.quickOpen')<CR>")
+-- keymap({ "n", "v" }, "<leader>cp", "<cmd>lua require('vscode').action('workbench.action.showCommands')<CR>")
+-- keymap({ "n", "v" }, "<leader>pr", "<cmd>lua require('vscode').action('code-runner.run')<CR>")
+-- keymap({ "n", "v" }, "<leader>fd", "<cmd>lua require('vscode').action('editor.action.formatDocument')<CR>")
 --
 -- harpoon keymaps
-keymap({ "n", "v" }, "<leader>ha", "<cmd>lua require('vscode').action('vscode-harpoon.addEditor')<CR>")
-keymap({ "n", "v" }, "<leader>ho", "<cmd>lua require('vscode').action('vscode-harpoon.editorQuickPick')<CR>")
-keymap({ "n", "v" }, "<leader>he", "<cmd>lua require('vscode').action('vscode-harpoon.editEditors')<CR>")
-keymap({ "n", "v" }, "<leader>h1", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor1')<CR>")
-keymap({ "n", "v" }, "<leader>h2", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor2')<CR>")
-keymap({ "n", "v" }, "<leader>h3", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor3')<CR>")
-keymap({ "n", "v" }, "<leader>h4", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor4')<CR>")
-keymap({ "n", "v" }, "<leader>h5", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor5')<CR>")
-keymap({ "n", "v" }, "<leader>h6", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor6')<CR>")
-keymap({ "n", "v" }, "<leader>h7", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor7')<CR>")
-keymap({ "n", "v" }, "<leader>h8", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor8')<CR>")
-keymap({ "n", "v" }, "<leader>h9", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor9')<CR>")
-
+-- keymap({ "n", "v" }, "<leader>H", "<cmd>lua require('vscode').action('vscode-harpoon.addEditor')<CR>")
+-- keymap({ "n", "v" }, "<leader>h", "<cmd>lua require('vscode').action('vscode-harpoon.editorQuickPick')<CR>")
+-- keymap({ "n", "v" }, "<leader>he", "<cmd>lua require('vscode').action('vscode-harpoon.editEditors')<CR>")
+-- keymap({ "n", "v" }, "<leader>1", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor1')<CR>")
+-- keymap({ "n", "v" }, "<leader>2", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor2')<CR>")
+-- keymap({ "n", "v" }, "<leader>3", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor3')<CR>")
+-- keymap({ "n", "v" }, "<leader>4", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor4')<CR>")
+-- keymap({ "n", "v" }, "<leader>5", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor5')<CR>")
+-- keymap({ "n", "v" }, "<leader>6", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor6')<CR>")
+-- keymap({ "n", "v" }, "<leader>7", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor7')<CR>")
+-- keymap({ "n", "v" }, "<leader>8", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor8')<CR>")
+-- keymap({ "n", "v" }, "<leader>9", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor9')<CR>")
+--
 -- project manager keymaps
-keymap({ "n", "v" }, "<leader>pa", "<cmd>lua require('vscode').action('projectManager.saveProject')<CR>")
-keymap({ "n", "v" }, "<leader>po", "<cmd>lua require('vscode').action('projectManager.listProjectsNewWindow')<CR>")
-keymap({ "n", "v" }, "<leader>pe", "<cmd>lua require('vscode').action('projectManager.editProjects')<CR>")
+-- keymap({ "n", "v" }, "<leader>pa", "<cmd>lua require('vscode').action('projectManager.saveProject')<CR>")
+-- keymap({ "n", "v" }, "<leader>po", "<cmd>lua require('vscode').action('projectManager.listProjectsNewWindow')<CR>")
+-- keymap({ "n", "v" }, "<leader>pe", "<cmd>lua require('vscode').action('projectManager.editProjects')<CR>")
