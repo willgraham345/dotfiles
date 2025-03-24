eaef768365b858b4ea0e1a07dbe30b7ea565923d
Will <willgraham345@gmail.com>
2025-03-24 17:03:01 -0600

    Changed colorscheme to sonokai, decided to use vscode for most stuff in the future

--- work/nvim/lua/plugins/colorscheme.lua
+++ work/nvim/lua/plugins/colorscheme.lua
@@ -26,6 +26,18 @@ return {
     },
   },
   { "projekt0n/github-nvim-theme", name = "github-theme" },
+  {
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      -- vim.g.sonokai_style = "atlantis"
+      vim.g.sonokai_enable_italic = true
+      vim.cmd.colorscheme("sonokai")
+    end,
+  },
 }
 
 -- return {
