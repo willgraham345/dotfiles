7de4b30ddb1cfbe00f43049ad754a93e5b2c91fe
Will <willgraham345@gmail.com>
2025-05-02 10:50:29 -0600

    image working, changed tmux passthrough

--- common/nvim/lua/config/lazy.lua
+++ common/nvim/lua/config/lazy.lua
@@ -15,7 +15,11 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  rocks = {
+    hererocks = true,
+  },
   spec = {
+    {"3rd/image.nvim", opts = {}},
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "sonokai" } },
     -- import/override with your plugins
@@ -51,5 +55,83 @@ require("lazy").setup({
     },
   },
 })
+
+require('image').setup({
+  integrations = {
+    markdown = {
+      resolve_image_path = function(document_path, image_path, fallback)
+        -- document_path is the path to the file that contains the image
+        -- image_path is the potentially relative path to the image. for
+        -- markdown it's `![](this text)`
+
+        -- you can call the fallback function to get the default behavior
+        return fallback(document_path, image_path)
+      end,
+    }
+  }
+})
+print(require("image").is_enabled()) -- bool
+
+-- require("image").setup({
+--   backend = "kitty",
+--   processor = "magick_cli", -- or "magick_rock"
+--   integrations = {
+--     markdown = {
+--       enabled = true,
+--       clear_in_insert_mode = false,
+--       download_remote_images = true,
+--       only_render_image_at_cursor = false,
+--       only_render_image_at_cursor_mode = "popup",
+--       floating_windows = false, -- if true, images will be rendered in floating markdown windows
+--       filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+--     },
+--     neorg = {
+--       enabled = true,
+--       filetypes = { "norg" },
+--     },
+--     typst = {
+--       enabled = true,
+--       filetypes = { "typst" },
+--     },
+--     html = {
+--       enabled = false,
+--     },
+--     css = {
+--       enabled = false,
+--     },
+--   },
+--   max_width = nil,
+--   max_height = nil,
+--   max_width_window_percentage = nil,
+--   max_height_window_percentage = 50,
+--   window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+--   window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" },
+--   editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+--   tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+--   hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+-- })
+
+-- require("diagram").setup({
+--   integrations = {
+--     require("diagram.integrations.markdown"),
+--     require("diagram.integrations.neorg"),
+--   },
+--   renderer_options = {
+--     -- mermaid = {
+--     --   theme = "forest",
+--     -- },
+--     -- plantuml = {
+--     --   charset = "utf-8",
+--     -- },
+--     d2 = {
+--       theme_id = 1,
+--     },
+--     -- gnuplot = {
+--     --   theme = "dark",
+--     --   size = "800,600",
+--     -- },
+--   },
+-- })
+
 
 -- Example from https://github.com/tanvirtin/vgit.nvim
