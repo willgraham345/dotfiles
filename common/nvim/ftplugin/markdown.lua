20c0510b8c294b53f270258e5050ff9c128628ea
Will <willgraham345@gmail.com>
2025-08-04 11:53:03 -0600

    Added ftplugin keymap for markdown to have shift+tab to de-indent

---
+++ common/nvim/ftplugin/markdown.lua
@@ -0,0 +1,9 @@
+-- ~/.config/nvim/ftplugin/markdown.lua
+print("Markdown ftplugin loaded!")
+
+-- Insert mode: Shift+Tab to de-indent
+vim.keymap.set("i", "<S-Tab>", "<C-d>", { buffer = true, desc = "De-indent in markdown" })
+-- Normal mode: Shift+Tab to unindent line
+vim.keymap.set("n", "<S-Tab>", "<<", { buffer = true, desc = "Normal de-indent in markdown" })
+-- Visual mode: Shift+Tab to unindent selection
+vim.keymap.set("v", "<S-Tab>", "<gv", { buffer = true, desc = "Visual de-indent in markdown" })
--- common/nvim/lua/plugins/rustaceanvim.lua
+++ common/nvim/lua/plugins/rustaceanvim.lua
@@ -1,26 +1,26 @@
 return {
   {
-    'mrcjkb/rustaceanvim',
-    version = '^6', -- Recommended
+    "mrcjkb/rustaceanvim",
+    version = "^6", -- Recommended
     lazy = false, -- This plugin is already lazy
     config = function()
-            -- local codelldb_path = vim.fn.stdpath('data') .. '/mason/bin/codelldb'
-            -- local liblldb_path = vim.fn.stdpath('data') .. '/mason/packages/codelldb/extension/lldb/lib/liblldb.dylib'
-            -- local cfg = require('rustaceanvim.config')
-            vim.g.rustaceanvim = {
-                -- dap = {
-                --     adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path)
-                -- },
-                server = {
-                    default_settings = {
-                        ['rust-analyzer'] = {
-                            runnables = {
-                                extraTestBinaryArgs = { "--nocapture" }
-                            }
-                        },
-                    },
-                },
-            }
-        end
-  }
+      -- local codelldb_path = vim.fn.stdpath('data') .. '/mason/bin/codelldb'
+      -- local liblldb_path = vim.fn.stdpath('data') .. '/mason/packages/codelldb/extension/lldb/lib/liblldb.dylib'
+      -- local cfg = require('rustaceanvim.config')
+      vim.g.rustaceanvim = {
+        -- dap = {
+        --     adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path)
+        -- },
+        server = {
+          default_settings = {
+            ["rust-analyzer"] = {
+              runnables = {
+                extraTestBinaryArgs = { "--nocapture" },
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
 }
