88e7f5859bdca8ddcee6616c7c214b886b39e13e
Will <willgraham345@gmail.com>
2025-08-05 08:46:44 -0600

    Added puml previewer, and updated <BS> command descriptions

--- common/nvim/lua/config/keymaps.lua
+++ common/nvim/lua/config/keymaps.lua
@@ -17,9 +17,9 @@ local map = vim.keymap.set
 -- Vim keymaps
 map({ "n", "v" }, "<BS><BS>", ":", { noremap = true, desc = "Start cmd dialog" })
 map({ "n", "v" }, "<BS>l", ":lua ", { noremap = true, desc = "Start lua command" })
-map({ "n", "v" }, "<BS>k", ":<C-p>", { noremap = true, desc = "Start lua command" })
+map({ "n", "v" }, "<BS>k", ":<C-p>", { noremap = true, desc = "Previous command" })
 map({ "n", "v" }, "<BS>c", "<Cmd>FzfLua commands<CR>", { noremap = true, desc = "Search commands" })
-map({ "n", "v" }, "<BS>h", "<Cmd>FzfLua help_tags<CR>", { noremap = true, desc = "Search commands" })
+map({ "n", "v" }, "<BS>h", "<Cmd>FzfLua help_tags<CR>", { noremap = true, desc = "Search help" })
 
 -- Window keymaps
 map("n", "<M-w>", "<C-w>q", { noremap = true, desc = "Kills the current window" })
---
+++ common/nvim/lua/plugins/puml_preview.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "weirongxu/plantuml-previewer.vim",
+    dependencies = {
+      "aklt/plantuml-syntax",
+      "tyru/open-browser.vim",
+    },
+    -- ft = { "plantuml", "puml", "uml" },
+    -- init = function()
+    --   vim.g.plantuml_previewer#debug_mode = 0     -- disable debug
+    --   vim.g.plantuml_previewer#save_format = "svg"
+    -- end,
+    -- opts = {},
+  },
+}
